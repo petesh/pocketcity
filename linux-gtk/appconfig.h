e198dbe1a05b6cc40a40743e08c4e7e645b9f646
petesh <petesh@ebce2e93-b627-0410-9732-a1265fc8941f>
2003-02-24 02:14:30 +0000

    Fix for compiling with the new fixed width types

--- linux-gtk/appconfig.h
+++ linux-gtk/appconfig.h
@@ -1,7 +1,22 @@
 #if !defined(_APPCONFIG_H)
 #define _APPCONFIG_H
 
-typedef struct _PalmAppConfig_01 {
+#if defined(__GNUC__) && defined(__UNIX__)
+	#pragma pack(2)
+#endif
+
+#if !defined(LP64)
+/* assume ILP32 ... terrible, I know */
+typedef signed char Int8;
+typedef unsigned char UInt8;
+typedef signed short Int16;
+typedef unsigned short UInt16;
+typedef signed int Int32;
+typedef unsigned int UInt32;
+#endif
+
+
+typedef struct _NixAppConfig_01 {
     int         window_x;
     int         window_y;
     int         window_width;
