4de5fdbaa73eac1d9fa7d019afb7b810adad4c37
petesh <petesh@ebce2e93-b627-0410-9732-a1265fc8941f>
2003-06-11 18:23:13 +0000

    savegame reads palm savegames ... need to repair some more

--- linux-gtk/savegame.c
+++ linux-gtk/savegame.c
@@ -13,6 +13,10 @@
 #include <handler.h>
 #include <ui.h>
 #include <simulation.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <inttypes.h>
+#include <strings.h>
 
 gchar * savegamename;
 
@@ -24,7 +28,91 @@ UIResetViewable(void)
     vgame.visible_y = 240/ vgame.tileSize;
 }
 
-void open_filename(GtkFileSelection *sel, gpointer data)
+void open_filename(GtkFileSelection *sel, gpointer data, int palm);
+
+void open_palmfilename(GtkFileSelection *sel, gpointer data)
+{
+    open_filename(sel, data, 1);
+}
+
+void open_platfilename(GtkFileSelection *sel, gpointer data)
+{
+    open_filename(sel, data, 0);
+}
+
+Int8
+read_int8(int fd)
+{
+    Int8 rv;
+    read(fd, &rv, 1);
+    return (rv);
+}
+
+Int16
+read_int16(int fd)
+{
+    Int8 by[2];
+    read(fd, by, 2);
+    return (by[0] << 8 | by[1]);
+}
+
+Int32
+read_int32(int fd)
+{
+    Int8 by[4];
+    read(fd, by, 4);
+    return (by[0] << 24 | by[1] << 16 | by[2] << 8 | by[3]);
+}
+
+void
+read_palmstructure(GameStruct *new, int fd)
+{
+    int i;
+
+    bzero(new, sizeof(*new));
+    read(fd, new->version, 4);
+    new->mapsize = read_int8(fd);
+    read_int8(fd);
+    /* skip visibles 2xint16 */
+    read_int32(fd);
+    new->map_xpos = read_int16(fd);
+    new->map_ypos = read_int16(fd);
+    /* skip cursor */
+    read_int32(fd);
+    new->credits = read_int32(fd);
+    /* build count */
+    i=0;
+    while (i++ < 20)
+	read_int32(fd);
+    new->TimeElapsed = read_int32(fd);
+    new->tax = read_int8(fd);
+    new->auto_bulldoze = read_int8(fd);
+    /* hole for name ?? */
+    read_int16(fd);
+    read(fd, new->cityname, CITYNAMELEN);
+    new->upkeep[0] = read_int8(fd);
+    new->upkeep[1] = read_int8(fd);
+    new->upkeep[2] = read_int8(fd);
+    new->diff_disaster = read_int8(fd);
+    i = 0;
+    while (i < 10) {
+	new->units[i].x = read_int16(fd);
+	new->units[i].y = read_int16(fd);
+	new->units[i].active = read_int16(fd);
+	new->units[i].type = read_int16(fd);
+	i++;
+    }
+    i = 0;
+    while (i < 10) {
+	new->objects[i].x = read_int16(fd);
+	new->objects[i].y = read_int16(fd);
+	new->objects[i].dir = read_int16(fd);
+	new->objects[i].active = read_int16(fd);
+	i++;
+    }
+}
+
+void open_filename(GtkFileSelection *sel, gpointer data, int palm)
 {
     int fd, ret;
     char tempversion[4];
@@ -53,13 +141,19 @@ void open_filename(GtkFileSelection *sel, gpointer data)
     
     /* God, I love to read all my vars at one time */
     /* using a struct :D ... unfortunately horribly unportable */
-    ret = read(fd, (void *)&game, sizeof(GameStruct));
-    if (ret == -1) {
-        perror("read game"); /* TODO: make this nicer */
-        return;
-    } else if (ret != sizeof(GameStruct)) {
-        g_print("Whoops, couldn't read full length of game\n");
-        return;
+    if (!palm) {
+    	ret = read(fd, (void *)&game, sizeof(GameStruct));
+	if (ret == -1) {
+	    perror("read game"); /* TODO: make this nicer */
+    	    return;
+    	} else if (ret != sizeof(GameStruct)) {
+    	    g_print("Whoops, couldn't read full length of game\n");
+    	    return;
+    	}
+    } else {
+        read_palmstructure(&game, fd);
+	/* make sure of position */
+	lseek(fd, 294, SEEK_SET);
     }
 
     /* and now the great worldPtr :D */
@@ -90,7 +184,7 @@ extern void UIOpenGame(GtkWidget *w, gpointer data)
 
     fileSel = gtk_file_selection_new("Select saved game to open");
     g_signal_connect(GTK_OBJECT(GTK_FILE_SELECTION(fileSel)->ok_button),
-            "clicked", G_CALLBACK(open_filename), (gpointer)fileSel);
+            "clicked", G_CALLBACK(open_palmfilename), (gpointer)fileSel);
 
     g_signal_connect_swapped(GTK_OBJECT(GTK_FILE_SELECTION(fileSel)->ok_button),
             "clicked", G_CALLBACK(gtk_widget_destroy), (gpointer)fileSel);
