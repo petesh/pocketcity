0624764276ab95f16c82a666b78c95bf7edf5e97
petesh <petesh@ebce2e93-b627-0410-9732-a1265fc8941f>
2004-03-15 14:55:57 +0000

    undid the stupid export of worldPtr and worldFlags, added get_mainwindow call for dialogs.

--- linux-gtk/main.h
+++ linux-gtk/main.h
@@ -4,6 +4,7 @@
  */
 
 #include <zakdef.h>
+#include <gtk/gtk.h>
 #include <gdk/gdk.h>
 
 #if !defined(_MAIN_H_)
@@ -13,17 +14,14 @@
 extern "C" {
 #endif
 
-/*! \brief externally exposed world Pointer (for savegames) */
-extern void *worldPtr;
-/*! \brief externally exposed world flags Pointer */
-extern void *worldFlagsPtr;
-
 void UIDrawMapField(Int16 xpos, Int16 ypos, welem_t nGraphic,
     GdkDrawable *drawable);
 void UIDrawMapSpecialObject(Int16 xpos, Int16 ypos, Int16 i,
     GdkDrawable *drawable);
 void UIDrawMapSpecialUnit(Int16 xpos, Int16 ypos, Int16 i,
     GdkDrawable *drawable);
+
+GtkWidget *mainwindow_get(void);
 
 #if defined(__cplusplus)
 }
