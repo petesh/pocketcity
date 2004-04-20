4b16ef34aa2342017f9710ac4e8305c5043d2397
petesh <petesh@ebce2e93-b627-0410-9732-a1265fc8941f>
2004-04-20 14:39:03 +0000

    add the minimap files (won't build without them)

---
+++ palm/minimap.c
@@ -0,0 +1,90 @@
+/*!
+ * \file
+ * This file contains the code for the on-screen minimap which is used to
+ * scroll around the real world
+ */
+
+#include <minimap.h>
+#include <zakdef.h>
+#include <globals.h>
+#include <simcity.h>
+#include <ui.h>
+#include <resCompat.h>
+#include <UIColor.h>
+
+static struct minimap {
+	RectangleType rect;
+	RectangleType rout;
+
+	Int8 showing:1;
+	Int8 detailed:1;
+} minimap;
+
+Int8
+minimapGetShowing(void)
+{
+	return (minimap.showing);
+}
+
+void
+minimapSetShowing(Int8 show)
+{
+	minimap.showing = show ? 1 : 0;
+}
+
+Int8
+minimapGetDetailed(void)
+{
+	return (minimap.detailed);
+}
+
+void
+minimapSetDetailed(Int8 detailed)
+{
+	minimap.detailed = detailed ? 1 : 0;
+}
+
+void
+minimapPlace(RectangleType *pos)
+{
+	MemMove(&minimap.rect, pos, sizeof (RectangleType));
+
+	if (minimap.rect.extent.x != 0 && minimap.rect.extent.y != 0) {
+		minimap.rout.extent.x = (Coord)((Int32)minimap.rect.extent.x *
+		    getVisibleX() / getMapWidth());
+		minimap.rout.extent.y = (Coord)((Int32)minimap.rect.extent.y *
+		    getVisibleY() / getMapHeight());
+	}
+}
+
+void
+minimapPaint(void)
+{
+	if (!minimap.showing) return;
+
+	WinEraseRectangle(&minimap.rect, 0);
+	minimap.rout.topLeft.x = minimap.rect.topLeft.x +
+	    (Int32)getMapXPos() / getMapWidth() * minimap.rect.extent.x;
+	minimap.rout.topLeft.y = minimap.rect.topLeft.y +
+	    (Int32)getMapYPos() / getMapHeight() * minimap.rect.extent.y;
+	WriteLog("Minimap rect = (%d,%d) -> (%d,%d)\n", minimap.rout.topLeft.x,
+	    minimap.rout.topLeft.y, minimap.rout.extent.x,
+	    minimap.rout.extent.y);
+	WinDrawRectangle(&minimap.rout, 0);
+}
+
+Int8
+minimapIsTapped(PointType *point, PointType *percentage)
+{
+	if (!minimap.showing) return (0);
+	if (RctPtInRectangle(point->x, point->y, &minimap.rect)) {
+		percentage->x = (Coord)(((Int32)point->x -
+		    minimap.rect.topLeft.x) * 100 /
+		    minimap.rect.extent.x);
+		percentage->y = (Coord)(((Int32)point->y -
+		    minimap.rect.topLeft.y) * 100 /
+		    minimap.rect.extent.y);
+		return (1);
+	}
+	return (0);
+}
---
+++ palm/minimap.h
@@ -0,0 +1,51 @@
+/*!
+ * \file
+ * interface to the minimap
+ */
+
+#include <PalmTypes.h>
+#include <Rect.h>
+
+/*!
+ * \brief is the minimap showing
+ * \return true if the minimap is showing
+ */
+Int8 minimapGetShowing(void);
+
+/*!
+ * \brief set whether the minimap is showing
+ * \param show set whether it is showing or not
+ */
+void minimapSetShowing(Int8 show);
+
+/*!
+ * \brief get the detailed flag (fast devices can render details)
+ * \return whether detailed minimaps are shown
+ */
+Int8 minimapGetDetailed(void);
+
+/*!
+ * \brief set detailed flag for minimap
+ * \param detailed the new setting for the detailed map
+ */
+void minimapSetDetailed(Int8 detailed);
+
+/*!
+ * \brief set the location of the minimap on the display
+ * \param pos the position and size of the minimap
+ */
+void minimapPlace(RectangleType *pos);
+
+/*!
+ * \brief paint the minimap if it is visible on the screen
+ */
+void minimapPaint(void);
+
+/*!
+ * \brief check if the minimap is tapped, returning the %age on each axis
+ * \param point the point to test
+ * \param percentage the percentages on the x and y axis
+ * \return non-zero if the map has been tapped, zero otherwise.
+ */
+Int8
+MinimapIsTapped(PointType *point, PointType *percentage);
