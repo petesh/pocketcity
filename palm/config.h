f9ce1fe7a80813f860ed76b5eeb803508aeae9b1
petesh <petesh@ebce2e93-b627-0410-9732-a1265fc8941f>
2004-08-30 18:38:49 +0000

    add file (breaks build)

---
+++ palm/config.h
@@ -0,0 +1,12 @@
+/*!
+ * \file
+ * \brief Platform level config file - palm
+ *
+ * This file contains all the #define commands for the palm platform.
+ */
+#if !defined(_CONFIG_H_)
+#define	_CONFIG_H_
+
+#define PALM
+
+#endif /* _CONFIG_H_ */
