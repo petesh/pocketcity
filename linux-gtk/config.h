ad2a3b6c66e2474831782d2c13d02a85ee56101b
petesh <petesh@ebce2e93-b627-0410-9732-a1265fc8941f>
2004-10-12 21:01:27 +0000

    Add the file - fix the build

---
+++ linux-gtk/config.h
@@ -0,0 +1,13 @@
+/*!
+ * \file
+ * \brief Platform level config file - l(u)n(i)x
+ *
+ * This file contains all the #define commands for the lunix platform.
+ * That's linux/unix to y'all
+ */
+#if !defined(_CONFIG_H_)
+#define _CONFIG_H_
+
+#define LUNIX
+
+#endif /* _CONFIG_H_ */
