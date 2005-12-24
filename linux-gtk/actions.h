aa5fa0ab409c8cd259732cbc9b878a26e55a7259
petesh <petesh@ebce2e93-b627-0410-9732-a1265fc8941f>
2005-12-24 15:20:08 +0000

    add the actions.h file

---
+++ linux-gtk/actions.h
@@ -0,0 +1,36 @@
+/*!
+ * \file
+ * \brief constant names for the actions - prevent typos
+ */
+
+#if !defined(_ACTIONS_H_)
+#define _ACTIONS_H_
+
+#define	BUDGET_SHOW	"budget-show"
+#define	MAP_SHOW	"map-show"
+#define	HOVER_SHOW	"hover-show"
+#define	A_SPEED_PAUSE	"speed-pause"
+#define	A_SPEED_SLOW	"speed-slow"
+#define	A_SPEED_MEDIUM	"speed-medium"
+#define	A_SPEED_FAST	"speed-fast"
+#define	A_SPEED_TURBO	"speed-turbo"
+#define A_BULLDOZER	"tool-bulldozer"
+#define A_ROAD		"tool-road"
+#define A_POWERLINE	"tool-power-line"
+#define A_RESIDENTIAL	"tool-residential"
+#define A_COMMERCIAL	"tool-commercial"
+#define A_INDUSTRIAL	"tool-industrial"
+#define A_TREE		"tool-tree"
+#define A_WATER		"tool-water"
+#define A_WATERPIPE	"tool-water-pipe"
+#define A_COALPLANT	"tool-power-plant"
+#define A_NUKEPLANT	"tool-nuclear-plant"
+#define	A_WATERPUMP	"tool-water-pump"
+#define	A_FIRESTATION	"tool-fire-station"
+#define A_POLICESTATION	"tool-police-station"
+#define	A_MILITARYBASE	"tool-military-base"
+#define A_FIREBRIGADE	"tool-defence-fire"
+#define	A_POLICECAR	"tool-police-car"
+#define A_TANK		"tool-tank"
+
+#endif /* _ACTIONS_H_ */
